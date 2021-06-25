a89aa8be7efde4d35687ab1bcc825a36c4ddf243
BoniehtRamirez <boniehtramirez@hotmail.com>
2021-06-25 14:59:45 -0600

    Commit inicial

---
+++ Examen2P2_BoniehtRamirez/src/examen2p2_boniehtramirez/Vehiculo.java
@@ -0,0 +1,46 @@
+package examen2p2_boniehtramirez;
+
+public class Vehiculo {
+    private String marca, modelo, carroceria;
+    private long VIN;
+
+    public Vehiculo(String marca, String modelo, String carroceria, long VIN) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.carroceria = carroceria;
+        this.VIN = VIN;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getCarroceria() {
+        return carroceria;
+    }
+
+    public void setCarroceria(String carroceria) {
+        this.carroceria = carroceria;
+    }
+
+    public long getVIN() {
+        return VIN;
+    }
+
+    public void setVIN(long VIN) {
+        this.VIN = VIN;
+    }
+     
+}
